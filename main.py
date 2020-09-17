d90352b9155dc350f046056fa77edab08f1ac34e
kpaqk <kpaqkpaq@gmail.com>
2020-09-17 20:09:24 +0300

    Second way

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 mnist = tf.keras.datasets.mnist
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train , x_test*5*2
+x_train, x_test = x_train , x_test
 model = tf.keras.models.Sequential([
  tf.keras.layers.Flatten(input_shape=(28, 28)),
  tf.keras.layers.Dense(512, activation=tf.nn.relu),
