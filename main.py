892693f58a30ab7cd9e5b284810338be53f24ca2
Alexander Sergeev <kpaqkpaq@gmail.com>
2020-09-24 12:54:09 +0300

    loss: 0.0867 - accuracy: 0.9806

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ mnist = tf.keras.datasets.mnist
 x_train, x_test = x_train /255, x_test/255
 model = tf.keras.models.Sequential([
  tf.keras.layers.Flatten(input_shape=(28, 28)),
- tf.keras.layers.Dense(512, activation=tf.nn.relu),
- tf.keras.layers.Dropout(0.2),
+ tf.keras.layers.Dense(256, activation=tf.nn.relu),
+ tf.keras.layers.Dropout(0.5),
  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 model.compile(optimizer='adam',
  loss='sparse_categorical_crossentropy',
  metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=5)
+model.fit(x_train, y_train, epochs=15)
 model.evaluate(x_test, y_test)
